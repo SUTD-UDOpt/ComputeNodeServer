539dab7d7d9b2ae2617b4de667bc8f1973cdb420
Anna Yenardi <69505589+AnnaYenardi@users.noreply.github.com>
2024-03-13 08:06:34 +0000

    updated areaDominated

--- controllers/types.ts
+++ controllers/types.ts
@@ -12,7 +12,7 @@ export interface FormData {
     firstLineType: number | null,
     allInitialEdgesAreRoad: number | null,
     culDeSacDepth: number | null,
-    areaDominated: boolean | null,
+    areaDominated: number | null,
     iPathOnly: number | null,
     weightContinuity: number,
     weightSideNumber: number,
